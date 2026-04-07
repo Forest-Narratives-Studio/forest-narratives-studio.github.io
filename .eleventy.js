3df3ebf4d6313f50be2b3fc7a90460d2e63b80ea
Dmitry I. Sokolov <d.i.sokolov@vk.com>
2026-04-07 14:50:55 +0400

    refactoring

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/styles.css");
   eleventyConfig.addPassthroughCopy("src/CNAME");
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/yandex_c0360f7b611be04f.html");
+  eleventyConfig.addPassthroughCopy("src/favicon.svg");
 
   return {
     dir: {
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,3 +1,9 @@
 module.exports = {
   url: "https://forestnarratives.studio",
+  name: "Forest Narratives Studio",
+  social: {
+    vk: "https://vk.com/forestnarrativesstudio",
+    telegram: "https://t.me/forestnarrativesstudio",
+    max: "https://max.ru/join/YQzEQSTrjVDh0yNAw5lNeJrT2PIjKEwnPiTQJtTaCIM",
+  },
 };
